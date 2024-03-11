e63ede9e65581a2f05580990f415f152379df62b
faaant <illiaeducation@gmail.com>
2024-03-11 12:53:07 +0200

    Update edge condition

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -13,7 +13,7 @@ def process_agent_data(
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
 
-    if agent_data.accelerometer.z>-100 and agent_data.accelerometer.z<100:
+    if 14000 > agent_data.accelerometer.z < 18000:
         return ProcessedAgentData(
             road_state = 'FINE',
             agent_data = agent_data
